e05cdd4cbd3cfd9223678dc37ca31bdde9ef42ad
ZinevichAndrey <tidra@bk.ru>
2020-09-07 23:15:38 +0300

    Update Fibanachi.java
    update

--- src/git/Fibanachi.java
+++ src/git/Fibanachi.java
@@ -2,16 +2,28 @@ package git;
 import java.math.BigInteger;
 
 public class Fibanachi {
-	//task ¹7 Create a Fibonacci array up to 20.
-	static int getFibanachi(int number) {
-		int firstNomber = 1;
-		int secondNomber = 1;
-		int result = 0;
+	//task ¹7 Create a Fibonacci method.
+	static BigInteger getFibanachi(int number) {
+		BigInteger result = BigInteger.valueOf(1);
+		BigInteger firstNomber = BigInteger.valueOf(1);
+		BigInteger secondNomber = BigInteger.valueOf(1);
 
+		if(number <= 0) {
+			return result.valueOf(0);
+		}
+		else if(number == 1) {
+			return result.valueOf(1);
+		}
+		else if(number == 2) {
+			return result.valueOf(1);
+		}
+		else if(number >= 46) {
+			throw new ArithmeticException();
+		}
 		for(int i = 3; i <= number; i++) {
-			result = firstNomber + secondNomber;
+			result = firstNomber.add(secondNomber);
 			firstNomber = secondNomber;
-			secondNomber = result;
+			secondNomber = result;	
 		}
 
 		return result;
